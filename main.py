47324c545058fd83ef2004db9971e8c2d2f0d06e
Diego Amorim <diegoamorim1912@gmail.com>
2025-03-12 18:27:46 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print('setup start')
+pygame.init()
+screen = pygame.display.set_mode(size=(800, 600))
+print('setup finish')
+
+print('loop starting')
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print('loop break event')
+            pygame.quit()
+            quit()
